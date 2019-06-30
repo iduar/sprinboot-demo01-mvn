feb31aa16753813ab9399fc3c8779fa54b9e30c9
iduar <raudi_619@hotmail.com>
2019-06-30 12:54:21 -0500

    Carga inicial
    Carga inicial proyecto demo01 con spring booot y maven

---
+++ src/main/java/mx/com/raudi/sprinboootdemo01mvn/SprinboootDemo01MvnApplication.java
@@ -0,0 +1,13 @@
+package mx.com.raudi.sprinboootdemo01mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinboootDemo01MvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinboootDemo01MvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/raudi/sprinboootdemo01mvn/SprinboootDemo01MvnApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.raudi.sprinboootdemo01mvn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprinboootDemo01MvnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
